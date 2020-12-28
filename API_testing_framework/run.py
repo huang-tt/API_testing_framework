3f65583e23c8a3ecd1b6825e4f4a9932e529b54c
huang-tt <75607641+huang-tt@users.noreply.github.com>
2020-12-28 13:51:41 +0800

    Update run.py

--- API_testing_framework/run.py
+++ API_testing_framework/run.py
@@ -9,7 +9,7 @@ import pytest
 if __name__ == '__main__':
     """执行并生成allure报告"""
     # pytest.main(["-s","-v",'test_case/test_gwyc_api_all.py','--alluredir','./report/result'])
-    pytest.main(["-s", "-v", 'test_case/test_api.py', '--alluredir', './report/result'])
+    pytest.main(["-s", "-v", 'test_case/test_api.py'])
 #     import os
 #     #读取json文件并生成HTML报告
 #     os.system('allure generate ./report/result -o ./report/html --clean')
